48bdb46b22074d689373f091cbaac92a383dbdcb
voed <voed@i.ua>
2025-06-25 22:06:07 +0300

    Update fan_schema

--- esphome/components/prana_ble/fan/__init__.py
+++ esphome/components/prana_ble/fan/__init__.py
@@ -18,8 +18,6 @@ from .. import (
     register_prana_child,
 )
 
-from esphome.cpp_generator import MockObjClass
-
 
 _LOGGER = logging.getLogger(__name__)
 CODEOWNERS = ["@voed"]
@@ -33,28 +31,6 @@ PRANA_FAN_TYPE = {
     "OUTLET": PranaFan.FAN_OUT,
     "BOTH": PranaFan.FAN_BOTH
 }
-_UNDEF = object()
-def fan_schema(
-    class_: MockObjClass = _UNDEF,
-    *,
-    entity_category: str = _UNDEF,
-    icon: str = _UNDEF,
-):
-    schema = cv.Schema({})
-    if class_ is not _UNDEF:
-        schema = schema.extend({cv.GenerateID(): cv.declare_id(class_)})
-    if entity_category is not _UNDEF:
-        schema = schema.extend(
-            {
-                cv.Optional(
-                    CONF_ENTITY_CATEGORY, default=entity_category
-                ): cv.entity_category
-            }
-        )
-    if icon is not _UNDEF:
-        schema = schema.extend({cv.Optional(CONF_ICON, default=icon): cv.icon})
-    return fan.FAN_SCHEMA.extend(schema)
-
 
 CONF_HAS_AUTO = "has_auto"
 CONF_FAN_TYPE = "fan_type"
@@ -69,15 +45,15 @@ ICON_FAN_CHEVRON_DOWN = "mdi:fan-chevron-down"
 CONFIG_SCHEMA = (
     cv.Schema(
         {
-            cv.Optional(CONF_FAN_IN): fan_schema(
+            cv.Optional(CONF_FAN_IN): fan.fan_schema(
                 PranaBLEFan,
                 icon=ICON_FAN_CHEVRON_DOWN
             ),
-            cv.Optional(CONF_FAN_OUT): fan_schema(
+            cv.Optional(CONF_FAN_OUT): fan.fan_schema(
                 PranaBLEFan,
                 icon=ICON_FAN_CHEVRON_UP
             ),
-            cv.Optional(CONF_FAN_BOTH): fan_schema(PranaBLEFan),
+            cv.Optional(CONF_FAN_BOTH): fan.fan_schema(PranaBLEFan),
             cv.Optional(CONF_SPEED_COUNT, default=10): cv.int_range(1, 10),
             cv.Optional(CONF_DIRECT, default=False): cv.boolean
         }
@@ -90,8 +66,7 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     if CONF_FAN_IN in config:
-        var = cg.new_Pvariable(config[CONF_FAN_IN][CONF_ID])
-        f = await fan.register_fan(var, config[CONF_FAN_IN])
+        var = await fan.new_fan(config[CONF_FAN_IN]);
         await cg.register_component(var, config)
         cg.add(var.set_fan_type(PranaFan.FAN_IN))
         cg.add(var.set_speed_count(config[CONF_SPEED_COUNT]))
@@ -99,8 +74,7 @@ async def to_code(config):
         await register_prana_child(var, config)
 
     if CONF_FAN_OUT in config:
-        var = cg.new_Pvariable(config[CONF_FAN_OUT][CONF_ID])
-        f = await fan.register_fan(var, config[CONF_FAN_OUT])
+        var = await fan.new_fan(config[CONF_FAN_OUT]);
         await cg.register_component(var, config)
         cg.add(var.set_fan_type(PranaFan.FAN_OUT))
         cg.add(var.set_speed_count(config[CONF_SPEED_COUNT]))
@@ -108,8 +82,7 @@ async def to_code(config):
         await register_prana_child(var, config)
 
     if CONF_FAN_BOTH in config:
-        var = cg.new_Pvariable(config[CONF_FAN_BOTH][CONF_ID])
-        f = await fan.register_fan(var, config[CONF_FAN_BOTH])
+        var = await fan.new_fan(config[CONF_FAN_BOTH]);
         await cg.register_component(var, config)
         cg.add(var.set_fan_type(PranaFan.FAN_BOTH))
         cg.add(var.set_speed_count(config[CONF_SPEED_COUNT]))
